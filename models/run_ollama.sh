22772b4c8ecde4789e1df70f37b878ee9500b9dc
Dmitrii Koriakov <dmitrii.koriakov@uni.lu>
2025-01-14 11:15:00 +0100

    update run_ollama.sh

--- models/run_ollama.sh
+++ models/run_ollama.sh
@@ -3,8 +3,14 @@
 echo "Starting Ollama server..."
 ollama serve &  # Start Ollama in the background
 
-echo "Ollama is ready, creating the model..."
+echo "Ollama is ready, creating the EuroLLM-9B-Instruct model..."
 
 ollama create EuroLLM-9B-Instruct -f models/EuroLLM-9B-Instruct/Modelfile
 
-echo "Ollama FINISH creating the model..."
+echo "Ollama FINISH creating the EuroLLM-9B-Instruct model..."
+
+echo "Ollama START pull the bge-m3 embedding model..."
+
+ollama pull bge-m3
+
+echo "Ollama FINISH pull the bge-m3 embedding model..."
