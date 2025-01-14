be5ed75110a5b8b4c25e1fb272be57e2979fdfab
Dmitrii Koriakov <dmitrii.koriakov@uni.lu>
2025-01-14 21:21:14 +0100

    mute custom

--- libs/ktem/ktem/app.py
+++ libs/ktem/ktem/app.py
@@ -39,8 +39,8 @@ class BaseApp:
 
     def __init__(self):
         self.dev_mode = getattr(settings, "KH_MODE", "") == "dev"
-        # self.app_name = getattr(settings, "KH_APP_NAME", "Web-RAG")
-        self.app_name = "Web-rag"
+        self.app_name = getattr(settings, "KH_APP_NAME", "Web-RAG")
+        print(f"app_name={self.app_name}")
         self.app_version = getattr(settings, "KH_APP_VERSION", "")
         self.f_user_management = getattr(settings, "KH_FEATURE_USER_MANAGEMENT", False)
         self._theme = KotaemonTheme()
