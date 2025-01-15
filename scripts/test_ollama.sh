e18c7b0f424a488cf1e9638c399bdc08bcecc7be
Dmitrii Koriakov <dmitrii.koriakov@uni.lu>
2025-01-15 12:54:20 +0100

    mute custom

--- scripts/test_ollama.sh
+++ scripts/test_ollama.sh
@@ -1,5 +1,5 @@
 curl http://localhost:11434/api/chat -d '{
-  "model": "EuroLLM-9B-Instruct",
+  "model": "DiiKorr/EuroLLM-9B-Instruct",
   "messages": [
     { "role": "user", "content": "de ce este cerul albastru?" }
   ]
