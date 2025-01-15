787bdc44cd5722751b5939227d5b32bc9fb3efc8
Dmitrii Koriakov <dmitrii.koriakov@uni.lu>
2025-01-15 19:16:33 +0100

    unmute readers[".pdf"] = docling_reader

--- libs/ktem/ktem/index/file/pipelines.py
+++ libs/ktem/ktem/index/file/pipelines.py
@@ -264,6 +264,7 @@ class DocumentRetrievalPipeline(BaseFileIndexRetriever):
             },
             "use_reranking": {
                 "name": "Use reranking",
+                "value": False,
                 "value": True,
                 "choices": [True, False],
                 "component": "checkbox",
