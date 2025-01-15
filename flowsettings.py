0d5c8bb237c2cc724cf15ba795b4f442dd42ec83
Dmitrii Koriakov <dmitrii.koriakov@uni.lu>
2025-01-15 16:06:06 +0100

    mute custom

--- flowsettings.py
+++ flowsettings.py
@@ -310,11 +310,13 @@ SETTINGS_REASONING = {
     },
 }
 
-USE_NANO_GRAPHRAG = config("USE_NANO_GRAPHRAG", default=True, cast=bool)
-USE_LIGHTRAG = config("USE_LIGHTRAG", default=True, cast=bool)
+# USE_NANO_GRAPHRAG = config("USE_NANO_GRAPHRAG", default=True, cast=bool)
+# USE_LIGHTRAG = config("USE_LIGHTRAG", default=True, cast=bool)
+USE_NANO_GRAPHRAG = False
+USE_LIGHTRAG = False
 
-GRAPHRAG_INDEX_TYPES = ["ktem.index.file.graph.GraphRAGIndex"]
-# GRAPHRAG_INDEX_TYPES = []
+# GRAPHRAG_INDEX_TYPES = ["ktem.index.file.graph.GraphRAGIndex"]
+GRAPHRAG_INDEX_TYPES = []
 
 if USE_NANO_GRAPHRAG:
     GRAPHRAG_INDEX_TYPES.append("ktem.index.file.graph.NanoGraphRAGIndex")
